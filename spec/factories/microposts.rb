0840e803eb223e9d0db55036d1493a9663473749
Peter Ang <peter.ang@me.com>
2012-08-27 09:25:28 -0400

    Add User Microposts

---
+++ spec/factories/microposts.rb
@@ -0,0 +1,10 @@
+# Read about factories at http://github.com/thoughtbot/factory_girl
+
+=begin
+FactoryGirl.define do
+  factory :micropost do
+    content "MyString"
+    user_id 1
+  end
+end
+=end
